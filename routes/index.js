1d5a6762d6aa914b03ddc33a3196ec72fad74788
David Tanner <darthtanner@gmail.com>
2014-05-25 16:44:13 -0600

    Adding logic to set pin state

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var gpio = require('rpi-gpio');
+gpio.setup(25, true);
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
@@ -9,6 +12,22 @@ router.get('/', function(req, res) {
 /* GET Hello World page. */
 router.get('/helloworld', function(req, res) {
     res.render('helloworld', { title: 'Hello, World!' })
+});
+
+router.get('/relay_change', function (req, res) {
+    var state = req.query.state != 0;
+    gpio.write(25, true, function(err) {
+        if (err) {
+            res.json({failed:true, wrote:-1});
+        } else {
+            res.json({failed:false, wrote:state});
+        }
+    })
+});
+
+
+router.get('/relay_off', function (req, res) {
+    res.json({wrote:1})
 });
 
 module.exports = router;
